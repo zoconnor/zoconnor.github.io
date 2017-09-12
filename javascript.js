fd8799c9447f276219a355f192647e6fd7153c5b
zoconnor <zach2794@live.missouristate.edu>
2017-09-12 14:14:09 -0500

    JS update #2

--- javascript.js
+++ javascript.js
@@ -1,32 +1,38 @@
-function validateForm() {
+function validateForm() 
+{
     var a = document.forms["contactForm"]["firstname"].value;
-    if (a==" ")
+    if (a == "")
         {
             alert("First Name must be filled out.");
             return false;
         }
     var b = document.forms["contactForm"]["lastname"].value;
-    else if (b==" ")
+    else if (b == "")
         {
             alert("Last Name must be filled out.");
             return false;
         }
     var c = document.forms["contactForm"]["email"].value;
-    else if(c==" ")
+    else if(c == "")
         {
             alert("Email must be filled out.");
             return false; 
         }
-      var d = document.forms["contactForm"]["contactreason"].value;
-    else if(d==" ")
+    var d = document.forms["contactForm"]["contactreason"].value;
+    else if(d == "")
         {
             alert("Reason must be filled out.");
             return false; 
         }
-      var e = document.forms["contactForm"]["subject"].value;
-    else if(e==" ")
+    var e = document.forms["contactForm"]["subject"].value;
+    else if(e == "")
         {
             alert("Subject must be filled out.");
             return false; 
         }
+}
+
+function clearForm()
+{
+    document.getElementById("contactForm").reset();
 }
