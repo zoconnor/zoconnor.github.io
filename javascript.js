e49b665c7a54cf80450b775f1060ef9eb0cfad9b
zoconnor <zach2794@live.missouristate.edu>
2017-09-19 16:34:03 -0500

    JS Updates

--- javascript.js
+++ javascript.js
@@ -8,28 +8,32 @@ function validateForm()
             return false;
         }
     var b = document.forms[contactForm][lastname].value;
-    else if (b == "")
+    if (b == "")
         {
             alert("Last Name must be filled out.");
             return false;
         }
     var c = document.forms[contactForm][email].value;
-    else if(c == "")
+    if(c == "")
         {
             alert("Email must be filled out.");
             return false; 
         }
     var d = document.forms[contactForm][contactreason].value;
-    else if(d == "")
+    if(d == "")
         {
             alert("Reason must be filled out.");
             return false; 
         }
     var e = document.forms[contactForm][subject].value;
-    else if(e == "")
+    if(e == "")
         {
             alert("Subject must be filled out.");
             return false; 
+        }
+    else
+        {
+            return true;
         }
 }
 
