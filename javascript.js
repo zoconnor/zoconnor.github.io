9d566e236f48cf38273e7e90c09066461a10e3eb
zoconnor <zach2794@live.missouristate.edu>
2017-09-19 16:55:21 -0500

    V3

--- javascript.js
+++ javascript.js
@@ -2,30 +2,31 @@
 function validateForm() 
 {
     var a = document.contactForm.firstname.value;
+    var b = document.contactForm.lastname.value;
+    var c = document.contactForm.email.value;
+    var d = document.contactForm.contactreason.value;
+    var e = document.contactForm.subject.value;
+
     if (a == "" || a == null)
         {
             alert("First Name must be filled out.");
             return false;
         }
-    var b = document.contactForm.lastname.value;
     else if (b == "" || b == null)
         {
             alert("Last Name must be filled out.");
             return false;
         }
-    var c = document.contactForm.email.value;
     else if(c == "" || c == null)
         {
             alert("Email must be filled out.");
             return false; 
         }
-    var d = document.contactForm.contactreason.value;
     else if(d == "" || d == null)
         {
             alert("Reason must be filled out.");
             return false; 
         }
-    var e = document.contactForm.subject.value;
     else if(e == "" || e == null)
         {
             alert("Subject must be filled out.");
