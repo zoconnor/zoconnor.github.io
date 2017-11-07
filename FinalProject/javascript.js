c6d86a9792f884eaac722ba03ae77564de0590c1
zoconnor <zach2794@live.missouristate.edu>
2017-11-07 15:24:03 -0600

    Container and Object Addition

--- FinalProject/javascript.js
+++ FinalProject/javascript.js
@@ -15,17 +15,3 @@ function switchLight(lightNumber) {
   light.classList.add("on");
 }
 
-/* Function to move object across screen */
-var imgObj = null;
-            
-function init(){
-imgObj = document.getElementById('myImage');
-imgObj.style.position= 'relative'; 
-imgObj.style.left = '0px'; 
-}
-            
-function moveRight(){
-    imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
-}
-            
-window.onload =init;
